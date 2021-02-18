005d42a1c079d36390d3f94664875665d5953770
epilogue <hubymagalie@loamok.org>
2021-02-18 04:04:24 +0100

    template,easy_admin,entite

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ConferenceRepository;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use Twig\Environment;
+use Symfony\Component\HttpFoundation\Request;
+
+class ConferenceController extends AbstractController
+{
+    private $twig;
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(ConferenceRepository $conferenceRepository)
+    {
+        return new Response( $this->twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+        
+    }
+    
+    /**
+     * @Route("/conference/{id}", name="conference")
+     * 
+     */
+    public function show(Request $request,Conference $conference, CommentRepository $commentRepository,ConferenceRepository $conferenceRepository)
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference,
+        $offset);
+        return new Response($this->twig->render('conference/show.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset +
+            CommentRepository::PAGINATOR_PER_PAGE),
+        ]));
+    }
+}
